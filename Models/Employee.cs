2f4fe2f417b926e94956747f745dd81a5a72e21b
TrungHa-IT <hatrung03022003@gmail.com>
2025-02-06 17:22:53 +0700

    AddModel

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChamCong.Models
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Role {  get; set; }
+        public string Image {  get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
